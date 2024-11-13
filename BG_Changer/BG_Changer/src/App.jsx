b33a24d947b40198145f4267a67443872e673ab0
Atul Kumar <atulmicrosft@gmail.com>
2024-11-13 14:47:47 +0530

    BG_Changer_Project

---
+++ BG_Changer/BG_Changer/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <body className='h-screen w-full' style={{backgroundColor: 'red'}}>
+
+    {/* <h1 className='flex justify-center text text-center'>My name is Atul Yadav</h1> */}
+
+    
+
+    </body>
+    </>
+  )
+}
+
+export default App
